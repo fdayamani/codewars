5a5a2d2051b50defd249413419602dabb28d764d
Fatema Damani <fatemadayamani@Fatemas-Air.home>
2016-07-02 14:44:43 +0100

    Vowel count implementation

--- src/main/java/vowelcount/Vowels.java
+++ src/main/java/vowelcount/Vowels.java
@@ -1,10 +1,38 @@
 package vowelcount;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Vowels {
+    static List<Character> vowels;
 
     public static int getCount(String str) {
         int vowelsCount = 0;
-        // your code here
+        char[] inputString = str.toCharArray();
+        determineVowels();
+
+        for (char character : inputString) {
+            vowelsCount = incrementIfVowel(vowelsCount, character);
+        }
+
+        return vowelsCount;
+    }
+
+    private static void determineVowels() {
+        vowels = new ArrayList<>();
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
+    }
+
+    private static int incrementIfVowel(int vowelsCount, char character) {
+        for (char vowel : vowels) {
+            if (character == vowel) {
+                vowelsCount++;
+            }
+        }
         return vowelsCount;
     }
 
