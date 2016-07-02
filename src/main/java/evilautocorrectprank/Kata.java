8d72ec63748b16ed0e8998cfcc35eef21eb169ba
Fatema Damani <fatemadayamani@Fatemas-Air.home>
2016-07-02 13:34:38 +0100

    Original Autocorrect kata files without modification

---
+++ src/main/java/evilautocorrectprank/Kata.java
@@ -0,0 +1,8 @@
+package evilautocorrectprank;
+
+public class Kata {
+    public static String autocorrect(String input) {
+        // your code here
+        return ""; // "corrected" input
+    }
+}
