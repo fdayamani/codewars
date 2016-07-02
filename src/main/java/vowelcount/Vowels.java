c82a08561bcdaede106a90c259a7a490f1044e8e
Fatema Damani <fatemadayamani@Fatemas-Air.home>
2016-07-02 14:29:10 +0100

    Original vowel count files without modification

---
+++ src/main/java/vowelcount/Vowels.java
@@ -0,0 +1,11 @@
+package vowelcount;
+
+public class Vowels {
+
+    public static int getCount(String str) {
+        int vowelsCount = 0;
+        // your code here
+        return vowelsCount;
+    }
+
+}
